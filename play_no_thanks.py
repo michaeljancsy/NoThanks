aab921980f8127359bbc1bfb4fa78c76a6c76d27
Michael Jancsy <michaeljancsy@Michaels-MacBook-Pro.local>
2015-11-23 13:12:46 -0800

    pep8

--- play_no_thanks.py
+++ play_no_thanks.py
@@ -1,89 +1,90 @@
 from itertools import cycle
 from random import sample, choice
 
+
 class Player(object):
-	def __init__(self):
-		self.hand = set()
-		self.chips = 11
+    def __init__(self):
+        self.hand = set()
+        self.chips = 11
 
-	def play(self, table, players):
-		'''
-		'play' calls 'take' or 'pass_'
+    def play(self, table, players):
+        '''
+        'play' calls 'take' or 'pass_'
 
-		This function is where game strategy can be implemented. 
-		'''
-		if self.chips:
-			random_play = choice([self.take, self.pass_])
-			random_play(table)
-		else:
-			self.take(table)
+        This function is where game strategy can be implemented.
+        '''
+        if self.chips:
+            random_play = choice([self.take, self.pass_])
+            random_play(table)
+        else:
+            self.take(table)
 
-	def take(self, table):
-		# take chips
-		self.chips += table.chips
-		table.chips = 0
-		# take card
-		self.hand.add(table.card)
-		table.card = table.cards.pop()
+    def take(self, table):
+        # take chips
+        self.chips += table.chips
+        table.chips = 0
+        # take card
+        self.hand.add(table.card)
+        table.card = table.cards.pop()
 
-	def pass_(self, table):
-		self.chips -= 1
-		table.chips += 1
+    def pass_(self, table):
+        self.chips -= 1
+        table.chips += 1
 
-	def score(self):
-		hand = sorted(self.hand)
-		score = -self.chips
-		index = 0
-		while index < len(hand):
-			if index == 0 or hand[index-1] != hand[index]-1:
-				score += hand[index]
-			index += 1
-		return score
+    def score(self):
+        hand = sorted(self.hand)
+        score = -self.chips
+        index = 0
+        while index < len(hand):
+            if index == 0 or hand[index-1] != hand[index]-1:
+                score += hand[index]
+            index += 1
+        return score
+
 
 class Players(object):
-	def __init__(self, num_players):
-		if (num_players < 3) or (num_players > 5):
-			raise ValueError("No Thanks requires 3 - 5 players.")
-		self.num_players = num_players
-		self._players = [Player() for _ in xrange(num_players)]
-		self.cycle = cycle(self._players)
+    def __init__(self, num_players):
+        if (num_players < 3) or (num_players > 5):
+            raise ValueError("No Thanks requires 3 - 5 players.")
+        self.num_players = num_players
+        self._players = [Player() for _ in xrange(num_players)]
+        self.cycle = cycle(self._players)
 
-	def next(self):
-		return self.cycle.next()
+    def next(self):
+        return self.cycle.next()
+
 
 class Table(object):
-	def __init__(self):
-		self.cards = None
-		self.card = None
-		self.chips = 0
+    def __init__(self):
+        self.cards = None
+        self.card = None
+        self.chips = 0
 
-	def deal(self):
-		deck = range(3, 36)
-		self.cards = sample(deck, 24)
-		self.card = self.cards.pop()
+    def deal(self):
+        deck = range(3, 36)
+        self.cards = sample(deck, 24)
+        self.card = self.cards.pop()
+
 
 def play_game(players, table):
-	table.deal()
-	while table.cards:
-		player = players.next()
-		player.play(table, players)
+    table.deal()
+    while table.cards:
+        player = players.next()
+        player.play(table, players)
+
 
 def determine_results(players):
-	player = players.next()
-	results = {}
-	while player not in results:
-		results[player] = player.score()
-		player = players.next()
-	return results
+    player = players.next()
+    results = {}
+    while player not in results:
+        results[player] = player.score()
+        player = players.next()
+    return results
 
 if __name__ == '__main__':
-	num_players = input('How many players?')
-	players = Players(num_players)
-	table = Table()
-	play_game(players, table)
-	results = determine_results(players)
-	print "Results: ", results
-
-
-
-
+    num_players = input('How many players?')
+    players = Players(num_players)
+    table = Table()
+    play_game(players, table)
+    results = determine_results(players)
+    print "Results: ", results
