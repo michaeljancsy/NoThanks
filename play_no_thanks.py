569540fecefde645e1bf24d593001a119bf57e99
Michael Jancsy <michaeljancsy@Michaels-MBP.lan>
2015-11-22 22:04:46 -0800

    initial commit

---
+++ play_no_thanks.py
@@ -0,0 +1,81 @@
+# goal: find optimal strategy for playing no thanks
+
+# optimization criterion: maximize entropy of outcomes
+
+# training method 1: probability of accepting card is estimated based on sigmoid function (trained via SGD?)
+
+from itertools import cycle
+from random import sample, choice
+
+class Player(object):
+	def __init__(self):
+		self.hand = []
+		self.chips = 11
+
+	def play(self, table, players):
+		'''
+		'play' calls 'take' or 'pass_'
+		'''
+		if self.chips:
+			s = choice([self.take, self.pass_])
+			s(table)
+		else:
+			self.take(table)
+
+	def take(self, table):
+		self.hand.append(table.card)
+		self.hand.sort()
+		table.card = table.cards.pop()
+
+	def pass_(self, table):
+		self.chips -= 1
+		table.chips += 1
+
+	def score(self):
+		score = -chips
+		index = 0
+		while index < len(self.hand):
+			if index != 0:
+				if self.hand[index-1] == self.hand[index]-1:
+					continue
+			else:
+				score += self.hand[index]
+		return score
+
+class Players(object):
+	def __init__(self, num_players):
+		self.num_players = num_players
+		self._players = [Player() for _ in xrange(num_players)]
+		self.cycle = cycle(self._players)
+
+	def next(self):
+		return self.cycle.next()
+
+class Table(object):
+	def __init__(self):
+		self.cards = None
+		self.card = None
+		self.chips = 0
+
+	def deal(self):
+		cards = range(3, 36)
+		dealt_cards = sample(cards, 24)
+		self.cards = dealt_cards
+		self.card = self.cards.pop()
+
+def play_game(players, table):
+	while table.cards:
+		player = players.next()
+		player.play(table, players)
+
+if __name__ == '__main__':
+	num_players = input('How many players?')
+	players = Players(num_players)
+	table = Table()
+	table.deal()
+	play_game(players, table)
+	print 'Done'
+
+
+
+
