1dd2b84310225d0326caea0034d57dc1dc6b540a
Michael Jancsy <michaeljancsy@gmail.com>
2015-11-27 11:32:05 -0500

    remove strategy method

--- code/strategies.py
+++ code/strategies.py
@@ -15,22 +15,3 @@ def default_strategy(self, table, players):
         random_play(table)
     else:
         self.take(table)
-
-
-def heuristic(self, table, players):
-    """
-    Simple strategy, still in development. Roughly, it takes a card if the
-    card's value is less than the average value of card's remaining on the
-    table, adjusting for the number of included chips.
-    """
-    # take if the marginal point value is less than 1 + the expected remainder
-    cards_in_hands = set()
-    for player in players.list_:
-        cards_in_hands.update(player.hand)
-    remaining_cards = set(table.cards) - cards_in_hands
-    mean_remaining_card_value = \
-        sum(remaining_cards) / len(remaining_cards) - table.chips
-    if table.card <= mean_remaining_card_value or self.chips == 0:
-        self.take(table)
-    else:
-        self.pass_(table)
